38541a3b6c7dd7b7de6aaf4c213861d20ce9eb4f
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-11-14 00:39:58 +0900

    implementopen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use open;
+use git2::{Repository};
+use structopt::StructOpt;
 
-fn main() {
-    open::that("http://rust-lang.org").unwrap();
+#[derive(StructOpt)]
+#[structopt(about = "Additional git tools")]
+enum GitPlus {
+    Open {name: Option<String>},
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = GitPlus::from_args();
+    match args {
+        GitPlus::Open { name } => {
+            let name = name.unwrap_or("origin".to_string());
+            let repo = Repository::open(".")?;
+            match repo.find_remote(name.as_str())?.url() {
+                Some(url) => open::that(url).unwrap(),
+                None => println!("No origin found"),
+            };   
+        }
+    }
+    Ok(())
 }
